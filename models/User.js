545f3b23d9aa02f6e6cf758ef4d145d39bc15c88
Osayemwenre Osaigbovo <122838434+Sayere1@users.noreply.github.com>
2025-11-05 22:31:21 +0100

    updated

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true},
     email: { type: String, required: true, unique: true},
     imageUrl : { type: String, required: true},
-    cartItem: { tpye: Object, default: {}}
+    cartItem: {type: Object, default: {}}
 }, {minimize: false})
 
 
