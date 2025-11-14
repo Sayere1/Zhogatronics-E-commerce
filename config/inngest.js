56595f8e38b083c45ded78e24f1e610318008dcc
Osayemwenre Osaigbovo <122838434+Sayere1@users.noreply.github.com>
2025-11-14 22:47:57 +0100

    update

--- config/inngest.js
+++ config/inngest.js
@@ -4,7 +4,7 @@ import User from "@/models/User";
 import Order from "@/models/order";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "zhogatronics" });
+export const inngest = new Inngest({ id: "zhogatronics-ecom" });
 
 
 // ingest function to save users data to mongoose dataBase
